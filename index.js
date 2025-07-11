6a781de770663973c4444208dca0934701b0c765
Karthick-7044 <karthick704400@gmail.com>
2025-07-11 16:41:42 +0530

    ]

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ console.log("MongoDB successfully connected");
 //middle ware
 app.use(express.json());
 app.use(cors({
-    origin:"http://localhost:3000"
+    origin:"https://crud-operation-two-sigma.vercel.app/"
 }))
 app.post("/create",async function(req,res){
     try{
